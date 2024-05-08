c7b1c567d75434868ea02073c46676d81c7bd4e2
kyle2501@gmail.com <kyle2501@gmail.com>
2024-05-08 07:50:17 -0700

    update serverFile_shopKeyboards.go

--- html-app/serverFile_shopKeyboards.go
+++ html-app/serverFile_shopKeyboards.go
@@ -20,3 +20,77 @@ import (
   
   
 )
+
+
+func open_browseShop() {
+    
+    
+}
+
+
+func openProduct_details() {
+    
+    
+}
+
+
+func addProduct_toCart() {
+    
+    
+}
+
+func create_newProduct() {
+    
+    
+}
+
+
+func editProduct_details() {
+
+
+
+}
+
+
+func open_shoppingCart() {
+    
+    
+}
+
+
+
+
+func main() {
+
+  appName := "SOS Bot App"
+  
+  http.HandleFunc("/", indexHandler)
+  
+  http.HandleFunc("/action/about", indexHandler)
+  
+   
+ 
+ http.HandleFunc("/homepage", indexHandler)
+    
+    
+    // -- -
+        port := os.Getenv("PORT")
+        if port == "" {
+                port = "8080"
+                log.Printf("Loading _webapp with default port")
+        }
+        log.Printf("_webapp is active and Listening on port %s", port)
+        
+        log.Printf("// -- - %s", appName)
+        log.Printf("_webapp now loaded and running at http://localhost:%s", port)
+        
+// -- - 
+        if err := http.ListenAndServe(":"+port, nil); err != nil {
+                log.Fatal("Error Starting the HTTP Server :", err)
+                return
+        }
+
+    
+    
+    
+}
